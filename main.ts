8057eea3ba6b74071c934764c561badc591acca3
sigmaangelo <angelo.frank.george@gmail.com>
2025-11-21 17:58:44 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,19 @@
-Deno.serve(async (req) => {
-  const url = new URL(req.url)
+import { serve } from "https://deno.land/std/http/server.ts";
 
+serve(async (req) => {
+  const url = new URL(req.url);
+
+  // When you visit "/"
   if (url.pathname === "/") {
     try {
-      const file = await Deno.readTextFile(".test.html")
+      const file = await Deno.readTextFile("./test.html");
       return new Response(file, {
         headers: { "content-type": "text/html" },
-      })
+      });
     } catch {
-      return new Response("File not found", { status: 404 })
+      return new Response("test.html not found", { status: 404 });
     }
   }
 
-  return new Response("Forbidden", { status: 403 })
-})
+  return new Response("404 - Not Found", { status: 404 });
+});
